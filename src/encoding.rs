5ebbc505ea0a03b7f0abd498c7fe3fff6496ba2a
Raman Sharma <techraman100@gmail.com>
2024-05-26 23:55:54 +0530

    Test Passed: Read File Route From Dir

---
+++ src/encoding.rs
@@ -0,0 +1,49 @@
+
+pub struct Encoding{
+    pub precentage_encode: fn(&str) -> String,
+    pub precentage_decode: fn(&str) -> String,
+    pub base64_encode: fn(&str) -> String,
+}
+
+
+impl Encoding {
+    #[allow(dead_code)]
+    pub fn precentage_encode(input: &str) -> String {
+        let mut encoded = String::new();
+        for byte in input.bytes() {
+            match byte {
+                0x30..=0x39 | 0x41..=0x5A | 0x61..=0x7A => {
+                    encoded.push(byte as char);
+                }
+                _ => {
+                    encoded.push('%');
+                    encoded.push_str(&format!("{:X}", byte));
+                }
+            }
+        }
+        encoded
+    }
+
+    pub  fn precentage_decode(input: &str) -> String {
+        let mut decoded = String::new();
+        let mut bytes = input.bytes();
+        while let Some(byte) = bytes.next() {
+            match byte {
+                0x25 => {
+                    let byte1 = bytes.next().unwrap();
+                    let byte2 = bytes.next().unwrap();
+                    let hex = u8::from_str_radix(&format!("{}{}", byte1 as char, byte2 as char), 16).unwrap();
+                    decoded.push(hex as char);
+                }
+                _ => {
+                    decoded.push(byte as char);
+                }
+            }
+        }
+        decoded
+    }
+
+    pub fn base64_encode(input: &str) -> String {
+        base64::encode(input)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 
 mod routes;
 mod request;
 mod response;
+mod encoding;
 
 use routes::Routes;
 use request::Request;
+use encoding::Encoding;
 use response::{Response, HTTPResponseStatus};
 
 fn main() {
@@ -60,6 +63,98 @@ fn setup_routes() -> Routes {
         response.headers.push("Content-Length: ".to_owned()+ &user_agent.len().to_string());
         response.send();
     }); 
+
+    routes.get("/files/:filename", |request,response|{
+
+        fn send500(response: &mut Response) {
+            response.status = HTTPResponseStatus::INTERNALSERVERERROR.to_string();
+            response.body = "500 Internal Server Error".to_string();
+            response.headers.push("Content-Type: text/plain".to_string());
+            response.headers.push("Content-Length: 21".to_string());
+            response.send();
+        }
+
+
+        let filename = request.params.get("filename").unwrap();
+        let env_args = std::env::args().collect::<Vec<String>>();
+        let dir = env_args.get(2).cloned().unwrap_or_else(|| {
+            send500(response);
+            return "".to_string();
+        });
+        let path = format!("{}/{}", dir, Encoding::precentage_decode(filename));
+
+        let binding = filename.split(".").collect::<Vec<&str>>();
+        let file_ext = binding.last().unwrap();
+
+        let content_type = match file_ext {
+            &"html" => "text/html",
+            &"css" => "text/css",
+            &"js" => "text/javascript",
+            &"png" => "image/png",
+            &"jpg" => "image/jpeg",
+            &"jpeg" => "image/jpeg",
+            &"gif" => "image/gif",
+            &"svg" => "image/svg+xml",
+            &"ico" => "image/x-icon",
+            &"json" => "application/json",
+            &"pdf" => "application/pdf",
+            _ => "application/octet-stream",
+        };
+
+        let isBinary = match file_ext {
+            &"png" => true,
+            &"jpg" => true,
+            &"jpeg" => true,
+            &"gif" => true,
+            &"svg" => true,
+            &"ico" => true,
+            &"pdf" => true,
+            _ => false,
+        };
+
+
+        if !fs::metadata(&path).is_ok() {
+            response.status = HTTPResponseStatus::NOTFOUND.to_string();
+            response.body = "404 Not Found".to_string();
+            response.headers.push("Content-Type: text/plain".to_string());
+            response.headers.push("Content-Length: 13".to_string());
+            response.send();
+            return;
+        }
+
+        if fs::metadata(&path).unwrap().is_dir() {
+            response.status = HTTPResponseStatus::FORBIDDEN.to_string();
+            response.body = "403 Forbidden".to_string();
+            response.headers.push("Content-Type: text/plain".to_string());
+            response.headers.push("Content-Length: 13".to_string());
+            response.send();
+            return;
+        }
+
+
+        
+
+        let contents = match fs::read(&path) {
+            Ok(contents) => contents,
+            Err(_) => {
+                send500(response);
+                return;
+            }
+        };
+
+        response.status = HTTPResponseStatus::OK.to_string();
+        response.body = if isBinary {
+            Encoding::base64_encode(
+                &String::from_utf8_lossy(&contents).to_string()
+            )
+        } else {
+            String::from_utf8_lossy(&contents).to_string()
+        };
+        response.headers.push("Content-Type: ".to_owned() + content_type);
+        response.headers.push("Content-Length: ".to_owned() + &contents.len().to_string());
+        response.send();
+
+    });
 
     routes
 }
